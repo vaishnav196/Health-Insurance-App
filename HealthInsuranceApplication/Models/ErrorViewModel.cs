4dc965f6591c788374f3087b40edecb9bcb74a24
vaishnav196 <vaish00721@gmail.com>
2024-06-28 12:54:44 +0530

    Add project files.

---
+++ HealthInsuranceApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealthInsuranceApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
